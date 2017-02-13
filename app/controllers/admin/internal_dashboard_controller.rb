e375aaa728a09b899a7ee324185ecff03d614cbc
Jasdeep Singh <narang.jasdeep@gmail.com>
2017-02-12 22:13:53 -0500

    display messages on the dashboard

--- app/controllers/admin/internal_dashboard_controller.rb
+++ app/controllers/admin/internal_dashboard_controller.rb
@@ -3,7 +3,8 @@ class Admin::InternalDashboardController < Admin::BaseController
   layout 'admin'
 
   def index
-  	@q = LawFirm.ransack(params[:q])
+    @q = LawFirm.ransack(params[:q])
+    @conversations = current_admin_user.mailbox.conversations
     @law_firms = @q.result(distinct: true).limit(5)
   end
 
