9484f362ac9a414affd9538cee4a7944746cbad7
Manpreet <manpreet@metawarelabs.com>
2017-03-05 12:40:10 -0500

    fixes active admin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf|woff2|css.erb)\z/
-Rails.application.config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf|woff2|js|css|css.erb)\z/
 
 
 # Add additional assets to the asset load path
