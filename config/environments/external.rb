ff9dd2c092f6ed33c181924f5821ab9abf5fbb79
Manpreet <manpreet@metawarelabs.com>
2017-03-20 11:18:42 -0400

    added fix

--- config/environments/external.rb
+++ config/environments/external.rb
@@ -79,6 +79,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.default_url_options = { host: 'seal.grcconsulting.ca' }
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
