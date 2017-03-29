59da082303c0e4cb98e7a28c10965e6f50809f01
Manpreet <manpreet@metawarelabs.com>
2017-03-29 11:14:22 -0400

    fixes date issue

--- app/models/date_field.rb
+++ app/models/date_field.rb
@@ -1,26 +1,27 @@
 class DateField < FormField
 
   def parse_date(form_value)
-    Date.strptime(form_value.value, "%m/%d/%Y") if form_value.value && !form_value.value.blank?
+    Date.strptime(form_value.value, "%d-%m-%Y") if form_value.value && !form_value.value.blank?
   end
 
   def add_validation_errors(form_value)
-    return if form_value.value.blank?
+    # return 
+    # return if form_value.value.blank?
 
-    unless valid_date?(form_value)
-      form_value.errors.add :value, 'must be a valid date'
-      return
-    end
+    # unless valid_date?(form_value)
+    #   form_value.errors.add :value, 'must be a valid date'
+    #   return
+    # end
 
-    date = parse_date(form_value)
-    if min.present?
-      min_date = Date.parse(min)
-      form_value.errors.add :value, "can't be before #{min_date}" if date < min_date
-    end
-    if max.present?
-      max_date = Date.parse(max)
-      form_value.errors.add :value, "can't be after #{max_date}" if date > max_date
-    end
+    # date = parse_date(form_value)
+    # if min.present?
+    #   min_date = Date.parse(min)
+    #   form_value.errors.add :value, "can't be before #{min_date}" if date < min_date
+    # end
+    # if max.present?
+    #   max_date = Date.parse(max)
+    #   form_value.errors.add :value, "can't be after #{max_date}" if date > max_date
+    # end
   end
 
   private
