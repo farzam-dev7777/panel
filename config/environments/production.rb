69a99abb4005aa9853a867c1c1b5bfa14a8f8937
Manpreet <manpreet@metawarelabs.com>
2019-11-14 12:13:45 -0500

    changed prod to mailtrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailjet
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '59a49ef19f604a',
+    :password => '6f8af86f36acb5',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
+
   # config.action_mailer.smtp_settings = {
   #   :address        => 'in-v3.mailjet.com',
   #   :port           => '587',
