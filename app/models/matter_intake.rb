da807b35fa149f06b3c2f69c9f896daf5033df48
Sandeep Bhatti <sandeep@metawarelabs.com>
2020-04-06 23:47:39 +0530

    minor typo fix

--- app/models/matter_intake.rb
+++ app/models/matter_intake.rb
@@ -93,7 +93,7 @@ class MatterIntake < ApplicationRecord
       actor_id: current_user.id,
       reviewable_type: self.class.to_s,
       reviewable_id: self.id,
-      description: "#{current_user.try(:full_name)} mark matter open in T360 with matter number #{self.matter_number}.",
+      description: "#{current_user.try(:full_name)} marked matter open in T360 with matter number #{self.matter_number}.",
       status: "matter_open"
     )
   end
