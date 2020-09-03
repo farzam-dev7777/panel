44465e5427bf32f9ee354e5ecec150c8a5374017
Sandeep Bhatti <sandeep@metawarelabs.com>
2020-09-03 19:24:08 +0530

    added corn job for docusign refresh token

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,3 +2,7 @@ every 1.day, :at => '4:30 am' do
   runner "QueuedNotification.send_notifications"
   runner "FormSubmission.check_for_expiry"
 end
+
+every 6.hours do
+  rake "rake docusign:refresh_token"
+end
