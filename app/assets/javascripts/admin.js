f799dc4d9950dbc879f41b668aec361627d7bbeb
Manish Chhipa <manishchhipa007@gmail.com>
2020-10-16 21:47:07 +0530

    fix : form enter issue fix

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -9,6 +9,16 @@ $(document).ready(function(){
 		}
 	})
 
+$('#login-form').each(function() {
+		$(this).find('input').keypress(function(e) {
+				// Enter pressed?
+				if(e.which == 10 || e.which == 13) {
+						this.form.submit();
+				}
+		});
+
+});
+
   $('.datetimepicker').datetimepicker();
 
 	// Create a new todo task
