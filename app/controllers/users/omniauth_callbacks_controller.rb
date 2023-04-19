8f83654af595f9639fe29d804be02d7d43376c51
Sandeep Bhatti <sandeep@metawarelabs.com>
2023-04-19 23:04:14 +0530

    minor change

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,5 +14,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def azure_activedirectory_v2
     @user = User.from_omniauth_azure(request.env["omniauth.auth"])
+    if @user&.id.present?
+      @user.reload
+      sign_in @user
+      redirect_to root_path
+    else
+      flash[:notice] = "Error authenticating! Please ensure user have permission to access."
+      redirect_to new_user_session_path
+    end
   end
 end
