fbd046fed5d1b7970ad412e26552f4a9f3fe7453
Manpreet Singh Narang <manpreet@metawarelabs.com>
2023-04-20 12:48:42 -0400

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,7 +22,7 @@ AZURE_SETUP = lambda do |env|
   env['omniauth.strategy'].options[:client_id] = tenant.azure_client_id          # if using azure-active-directory-2
   env['omniauth.strategy'].options[:client_secret] = tenant.azure_client_secret   # if using azure-active-directory-2
   env['omniauth.strategy'].options[:tenant_id] = tenant.azure_tenant_id   # if using azure-active-directory-2
-  env['omniauth.strategy'].options['token_params'] = {:redirect_uri => 'https://lb.preseal.ca/users/auth/azure_activedirectory_v2/callback'}
+  env['omniauth.strategy'].options['redirect_uri'] = 'https://lb.preseal.ca/users/auth/azure_activedirectory_v2/callback'
 end
 
 Devise.setup do |config|
