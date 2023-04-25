a48d80baf5f41de79a3761a35101b4371cda85ac
Manpreet <manpreetrules@gmail.com>
2023-04-25 17:55:28 -0400

    fixes multiTenant

--- app/models/panel_request.rb
+++ app/models/panel_request.rb
@@ -60,7 +60,8 @@ class PanelRequest < ApplicationRecord
     "None of the above"
   ]
 
-  validates_presence_of :submitted_by_email, :line_of_business, :women_owned, :niche_preferred_external_counsel_panel_law_firms, :matter_types, :required_unique_geography, :involved_engagement, :women_owned, :law_frim_name, :law_firm_contact_name, :law_firm_mail, :law_firm_role, :law_firm_phone, :firm_use_on_regular_basis
+  validates_presence_of :submitted_by_email
+  #, :line_of_business, :women_owned, :niche_preferred_external_counsel_panel_law_firms, :matter_types, :required_unique_geography, :involved_engagement, :women_owned, :law_frim_name, :law_firm_contact_name, :law_firm_mail, :law_firm_role, :law_firm_phone, :firm_use_on_regular_basis
 
 
   def matter_types
