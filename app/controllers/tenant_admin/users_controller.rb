971b3111a6126e6a47ca1db256316715fe18ecb0
Manpreet <manpreetrules@gmail.com>
2023-06-09 13:46:16 -0400

    fixes law firm name

--- app/controllers/tenant_admin/users_controller.rb
+++ app/controllers/tenant_admin/users_controller.rb
@@ -51,8 +51,8 @@ class TenantAdmin::UsersController < TenantAdmin::BaseController
   end
 
   def reset_password_instructions
-    user = User.find_by(id: params[:id])
-    user.send_reset_password_instructions
+    @resource = User.find_by(id: params[:id])
+    @resource.send_reset_password_instructions
     flash[:notice] = "User Password Reset email sent successfully."
     redirect_to tenant_admin_users_path
   end
