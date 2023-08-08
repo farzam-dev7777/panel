bb091cfe106155254226768a11736ad994b84ad4
kaushal <kaushalk16@gmail.com>
2023-08-08 20:43:38 +0530

    Added Forgot password in login screen

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2555,4 +2555,11 @@ $('#matter_intake_status').chosen().change(function() {
 		}
 	});
 
+	$('.login-btn').on('click', function() {
+		if($("#tmc_checkbox").is(':checked') == false){
+			alert('Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy')
+			return false;
+		}
+	});
+
 });
