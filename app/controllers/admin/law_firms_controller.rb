ab0ead4f37c5b1ca357dc732bbcb62309b99c63d
kaushal <kaushalk16@gmail.com>
2023-10-30 22:30:11 +0530

    Updated begin_recertification_process to handle issues

--- app/controllers/admin/law_firms_controller.rb
+++ app/controllers/admin/law_firms_controller.rb
@@ -144,49 +144,49 @@ class Admin::LawFirmsController < Admin::BaseController
     new_form_submission.expiry_date = nil
 
     Form.where(step: 'conflicts')&.last&.form_fields&.each do |form_field|
-      next if new_form_submission.form.try(:form_fields).map(&:label).include? form_field.label
+      next if new_form_submission.form.try(:form_fields)&.map(&:label).include? form_field.label
       ff = form_field.amoeba_dup
       ff.formable_id = new_form_submission.form.id
       ff.save
     end
 
     Form.where(step: 'pricing')&.last&.form_fields&.each do |form_field|
-      next if new_form_submission.form.try(:form_fields).map(&:label).include? form_field.label
+      next if new_form_submission.form.try(:form_fields)&.map(&:label).include? form_field.label
       ff = form_field.amoeba_dup
       ff.formable_id = new_form_submission.form.id
       ff.save
     end
 
     Form.where(step: 'relationship')&.last&.form_fields&.each do |form_field|
-      next if new_form_submission.form_relationship.try(:form_fields).map(&:label).include? form_field.label
+      next if new_form_submission.form_relationship.try(:form_fields)&.map(&:label).include? form_field.label
       ff = form_field.amoeba_dup
       ff.formable_id = new_form_submission.form.id
       ff.save
     end
 
     Form.where(step: 'diversity')&.last&.form_fields&.each do |form_field|
-      next if new_form_submission.form_diversity.try(:form_fields).map(&:label).include? form_field.label
+      next if new_form_submission.form_diversity.try(:form_fields)&.map(&:label).include? form_field.label
       ff = form_field.amoeba_dup
       ff.formable_id = new_form_submission.form.id
       ff.save
     end
 
     Form.where(step: 'innovation')&.last&.form_fields&.each do |form_field|
-      next if new_form_submission.form_innovation.try(:form_fields).map(&:label).include? form_field.label
+      next if new_form_submission.form_innovation.try(:form_fields)&.map(&:label).include? form_field.label
       ff = form_field.amoeba_dup
       ff.formable_id = new_form_submission.form.id
       ff.save
     end
 
     Form.where(step: 'resourcing')&.last&.form_fields&.each do |form_field|
-      next if new_form_submission.form_resourcing.try(:form_fields).map(&:label).include? form_field.label
+      next if new_form_submission.form_resourcing.try(:form_fields)&.map(&:label).include? form_field.label
       ff = form_field.amoeba_dup
       ff.formable_id = new_form_submission.form.id
       ff.save
     end
 
     Form.where(step: 'lawfirm')&.last&.form_fields&.each do |form_field|
-      next if new_form_submission.form_lawfirm.try(:form_fields).map(&:label).include? form_field.label
+      next if new_form_submission.form_lawfirm.try(:form_fields)&.map(&:label).include? form_field.label
       ff = form_field.amoeba_dup
       ff.formable_id = new_form_submission.form.id
       ff.save
