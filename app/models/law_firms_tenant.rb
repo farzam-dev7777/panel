05813d57d5282ab973f4657dd2564902dc6f9080
kaushal <kaushalk16@gmail.com>
2023-11-08 22:14:34 +0530

    Updated law firm show page to add 'Allow to create matters'

--- app/models/law_firms_tenant.rb
+++ app/models/law_firms_tenant.rb
@@ -5,4 +5,12 @@ class LawFirmsTenant < ApplicationRecord
 
   mount_uploader :document, FileUploader
   serialize :type_of_matters_your_law_firm_handles_for_us, Array
+
+  def allow_to_matters
+    if allow_to_create_matters == true
+      'Yes'
+    elsif allow_to_create_matters == false
+      'No'
+    end
+  end
 end
