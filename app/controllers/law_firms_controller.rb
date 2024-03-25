56fa532d2c6186d03b5bc305a5bd7e3411475c3f
kaushal <kaushalk16@gmail.com>
2024-03-25 20:53:19 +0530

    Updated get_external_lawyers api to render law firm users

--- app/controllers/law_firms_controller.rb
+++ app/controllers/law_firms_controller.rb
@@ -124,7 +124,7 @@ class LawFirmsController < BaseController
   def get_external_lawyers
     law_firm = LawFirm.find_by_id params[:id]
     if law_firm.present?
-      lawyers = law_firm.external_lawyers.map{|s| {id:s.id, name: s.name}}
+      lawyers = law_firm.users.where(role:'user').map{|s| {id:s.id, name: s.name}}
       render json: {users: lawyers}
     else
       render json: {users: []}
