5b53ac03d5358b3e88af193a471caa2aa55a732a
farzam-dev7777 <farzam.dev7777@gmail.com>
2026-08-24 12:44:32 +0500

    Minor CHnage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
 if Rails.env.production? || Rails.env.external?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider:               'AWS',
-      aws_access_key_id:      ENV['DO_ACCESS_KEY'],
-      aws_secret_access_key:  ENV['DO_SECRET_KEY'],
-      region:                 ENV['DO_REGION']
+      provider:                        'AWS',
+      aws_access_key_id:               ENV['DO_ACCESS_KEY'],
+      aws_secret_access_key:           ENV['DO_SECRET_KEY'],
+      region:                          ENV['DO_REGION'],
+      endpoint:                        ENV['DO_ENDPOINT'] || "https://#{ENV['DO_REGION']}.digitaloceanspaces.com",
+      enable_signature_v4_streaming:   false
     }
 
     config.fog_directory    = ENV['DO_BUCKET_NAME']
@@ -13,10 +15,12 @@ if Rails.env.production? || Rails.env.external?
 elsif Rails.env.staging? || Rails.env.internal? || Rails.env.development?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider:               'AWS',
-      aws_access_key_id:      ENV['DO_ACCESS_KEY'],
-      aws_secret_access_key:  ENV['DO_SECRET_KEY'],
-      region:                 ENV['DO_REGION']
+      provider:                        'AWS',
+      aws_access_key_id:               ENV['DO_ACCESS_KEY'],
+      aws_secret_access_key:           ENV['DO_SECRET_KEY'],
+      region:                          ENV['DO_REGION'],
+      endpoint:                        ENV['DO_ENDPOINT'] || "https://#{ENV['DO_REGION']}.digitaloceanspaces.com",
+      enable_signature_v4_streaming:   false
     }
 
     config.fog_directory    = ENV['DO_BUCKET_NAME']
