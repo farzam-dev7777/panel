f7eba2db3517c8ad18005e8d5c75a3f60d798e48
farzam-dev7777 <farzam.dev7777@gmail.com>
2026-08-27 16:15:32 +0500

    Minor Domain Adjustment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_dispatch.tld_length = 4
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
